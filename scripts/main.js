c1f0cd8222de062c65e8744bb1cca8e86958e6c3
Gavenmore <jamesdon10@hotmail.com>
2019-03-23 15:27:33 -0600

    Beautified site

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,26 @@
 
 //This is for populating the 'header' with the content of the header html file.
-$(function () {
-    $("header").load("header.html"); 
-});
+// $(function () {
+//     $("header").load("header.html"); 
+// });
 
 //This is for populating the 'mainNavigation' with the content of the navigation html file.
 $(function () { 
-    $("#fancyNav").load("main-nav.html"); 
+    $("header").load("header.html"); 
+    $("#fancyNav").load("main-nav.html");
 });
+
+$(document).ready(function () {
+    // $("header").load("header.html"); 
+    // $("#fancyNav").load("main-nav.html");
+
+    if (window.location.hash) {
+        $('html, body').animate({
+            scrollTop: $(window.location.hash).offset().top - 20
+        }, 500);
+    }
+});
+
 
 // This is for detaching the 'mainNavigation' when it reaches the footer.
 var fancyNav = $("#fancyNav");
