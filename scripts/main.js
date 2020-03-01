d9d031f2872e013380f17de65f05a715c3d420e9
Gavenmore <jamesdon10@hotmail.com>
2020-03-01 16:59:25 -0700

    Got the 'new' element to get appended to new index page news posts

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 
 //This is for populating the 'mainNavigation' with the content of the navigation html file.
-$(function () { 
-    $("header").load("header.html"); 
+$(function () {
+    $("header").load("header.html");
     $("#fancyNav").load("main-nav.html");
+    CheckDate();
 });
 
 
@@ -139,3 +140,55 @@ ReplaceAnchors=function(){
     }
 }
 
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+mm = today.toLocaleString('default', { month: 'long' });
+var lastPost = new Date();
+var dateArray;
+var postToCheck;
+//month, day year format
+var date1 = new Date();
+var date2 = new Date();
+var diffTime;
+var diffDays;
+var newElement;
+CheckDate=function() {
+    allArticles = document.getElementsByClassName("date");
+    //Get the date
+    for (var i = 0; i < allArticles.length; i++) {
+        lastPost = allArticles[i].innerHTML;
+        dateArray = lastPost.split(' ');
+        dateArray[0] = dateArray[0].substring(0,3);
+
+        if (dateArray[1].length < 3) {
+            dateArray[1] = ("0" + dateArray[1]).substring(0,2);
+        } else {
+            dateArray[1] = dateArray[1].substring(0,2);
+        }
+
+        //format the string for the date
+        postToCheck = dateArray[0]
+                                + "/"
+                                + dateArray[1]
+                                + "/"
+                                + dateArray[2];
+
+
+        //month, day, year
+        date1 = new Date(postToCheck);
+        date2 = today;
+        diffTime = Math.abs(date2 - date1);
+        diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        if (diffDays <= 7) {
+            console.log("post " + i + " is new!");
+            document.getElementsByClassName("date")[i].parentElement.parentElement
+                .appendChild(newElement = document.createElement("p"));
+                newElement.innerHTML = "New!";
+                newElement.className += "new-symbol";
+        }//end if
+
+    }//end for i loop
+}//end CheckDate
