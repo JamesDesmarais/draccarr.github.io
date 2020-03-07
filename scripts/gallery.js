c0ecca84d9ec44c0c00069f24edcedd40e1da79c
Gavenmore <jamesdon10@hotmail.com>
2020-03-07 02:43:47 -0700

    Got infinite grids working!

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -8,97 +8,97 @@ var ig = new InfiniteGrid(document.getElementById("Grid"), {
     horizontal: false,
 });
 
-// // initialize layout
-// // GridLayout, JustifiedLayout, FrameLayout, SquareLayout, PackingLayout
+// initialize layout
+// GridLayout, JustifiedLayout, FrameLayout, SquareLayout, PackingLayout
 ig.setLayout(GridLayout, {
     itemSize: 300,
     align: "center"
 });
 
-// ig.append("<li><img id=\"0\" src=\"./images/jay-symbol-300x300.png\" width=\"300\" height=\"300\" alt=\"Jay symbol By James Desmarais\" title=\"Jay symbol By James Desmarais\"></li><li><img id=\"1\" src=\"./images/cave-background-300x300.png\" width=\"300\" height=\"300\" alt=\"Cave Background Image By James Desmarais\" title=\"Cave Background Image By James Desmarais\"></li><li><img id=\"2\" src=\"./images/main-menu-background-170x170.png\" width=\"170\" height=\"170\" alt=\"Pixel Art Sunset By James Desmarais\" title=\"Pixel Art Sunset By James Desmarais\"></li><li><img id=\"3\" src=\"./images/praedae-the-pirate-head-300x300.png\" width=\"300\" height=\"300\" alt=\"Praedae The Pirate By James Desmarais\" title=\"Praedae The Pirate By James Desmarais\"></li><li><img id=\"4\" src=\"./images/daiki-ibutsu-300x418.png\" width=\"300\" height=\"418\" alt=\"Daiki Ibutsu By James Desmarais\" title=\"Daiki Ibutsu By James Desmarais\"></li><li><img id=\"5\" src=\"./images/robot-game-300x188.png\" width=\"300\" height=\"300\" alt=\"Robot Game Image By James Desmarais\" title=\"Robot Game Image By James Desmarais\"></li><li><img id=\"6\" src=\"./images/red-blue-eye-300x300.png\" width=\"300\" height=\"300\" alt=\"Red Blue Eye By James Desmarais\" title=\"Red Blue Eye By James Desmarais\"></li><li><img id=\"7\" src=\"./images/building-1-full-with-closed-door-300x193.png\" width=\"300\" height=\"193\" alt=\"Pixel Art Building By James Desmarais\" title=\"Pixel Art Building By James Desmarais\"></li><li><img id=\"8\" src=\"./images/vatherdone-with-tree-ghosts.png\" width=\"300\" height=\"193\" alt=\"Pixel Art Building By James Desmarais\" title=\"Pixel Art Building By James Desmarais\"></li>");
-
-// var template = '<div class="item">' + //original
-//                                 '<div class="thumbnail">' + 
-//                                     '<img src="${link}../../assets/image/${no}.jpg">' + 
-//                                 '</div>' + 
-//                                 '<div class="info">' + 
-//                                     '${text}' + 
-//                                 '</div>' + 
-//                             '</div>';
-// var template = '<li>' 
-//                             + '<img src="${link}${no}.png">' 
-//                         + '<li>';
-
-//ToDo: 
-// make an array of image names
-// cycle through the images
-class HoldDirectory {
-    Directory;
-    HoldDirectory() {
-
-    }//end constructor
-    HoldDirectory(_directory) {
-        Directory = _directory;
-    }//end constructor
-}
-var folder = "./images";
-var directories = []; 
-var currentDir;
-$.ajax({
-    url: folder,
-    success: function (data) {
-        $(data).find("a").attr("href", function (i, val) {
-            if (val.match(/\.(jpe?g|png|gif)$/)) {
-                directories.push(currentDir = new HoldDirectory(val));
-                console.log(currentDir);
-            }
-        });
-    }
-});
-// console.log(directories.length);
-console.log(directories.length);
-for(var i=0; i< directories.length; i++) {
-}
+var galleryDoc;
+var allGalleryPosts;
+var currentGalleryPost;
+$.get("https://draccarr.github.io/gallery-content.html", function (_content) {
+    galleryDoc = new DOMParser().parseFromString(_content, "text/html");
+    allGalleryPosts = galleryDoc.getElementsByTagName("li");
 
 
-var template = '<li><img src="${link}job-board.png"><li>';
-var link = "https://draccarr.github.io/images/";
-function getItem(_template, _options) {
-    // return template.replace(/\$\{([^\}]*)\}/g, function () { //original
-    return _template.replace(/\$\{([^\}]*)\}/g, function () {
-        var replaceTarget = arguments[1];
 
-        return _options[replaceTarget];
-    }//end function
-    );
-}//end getItem
 
-function getItems(_length) {
-    var arr = [];
+    // ig.append("<li><img id=\"0\" src=\"./images/jay-symbol-300x300.png\" width=\"300\" height=\"300\" alt=\"Jay symbol By James Desmarais\" title=\"Jay symbol By James Desmarais\"></li><li><img id=\"1\" src=\"./images/cave-background-300x300.png\" width=\"300\" height=\"300\" alt=\"Cave Background Image By James Desmarais\" title=\"Cave Background Image By James Desmarais\"></li><li><img id=\"2\" src=\"./images/main-menu-background-170x170.png\" width=\"170\" height=\"170\" alt=\"Pixel Art Sunset By James Desmarais\" title=\"Pixel Art Sunset By James Desmarais\"></li><li><img id=\"3\" src=\"./images/praedae-the-pirate-head-300x300.png\" width=\"300\" height=\"300\" alt=\"Praedae The Pirate By James Desmarais\" title=\"Praedae The Pirate By James Desmarais\"></li><li><img id=\"4\" src=\"./images/daiki-ibutsu-300x418.png\" width=\"300\" height=\"418\" alt=\"Daiki Ibutsu By James Desmarais\" title=\"Daiki Ibutsu By James Desmarais\"></li><li><img id=\"5\" src=\"./images/robot-game-300x188.png\" width=\"300\" height=\"300\" alt=\"Robot Game Image By James Desmarais\" title=\"Robot Game Image By James Desmarais\"></li><li><img id=\"6\" src=\"./images/red-blue-eye-300x300.png\" width=\"300\" height=\"300\" alt=\"Red Blue Eye By James Desmarais\" title=\"Red Blue Eye By James Desmarais\"></li><li><img id=\"7\" src=\"./images/building-1-full-with-closed-door-300x193.png\" width=\"300\" height=\"193\" alt=\"Pixel Art Building By James Desmarais\" title=\"Pixel Art Building By James Desmarais\"></li><li><img id=\"8\" src=\"./images/vatherdone-with-tree-ghosts.png\" width=\"300\" height=\"193\" alt=\"Pixel Art Building By James Desmarais\" title=\"Pixel Art Building By James Desmarais\"></li>");
 
-    for (var i = 0; i < _length; ++i) {
-        arr.push(getItem(template, { no: i % 60 + 1, text: "egjs post " + (i + 1), link: link }));
-    }
-    return arr;
-}
+    // var template = '<div class="item">' + //original
+    //                                 '<div class="thumbnail">' + 
+    //                                     '<img src="${link}../../assets/image/${no}.jpg">' + 
+    //                                 '</div>' + 
+    //                                 '<div class="info">' + 
+    //                                     '${text}' + 
+    //                                 '</div>' + 
+    //                             '</div>';
+    // var template = '<li>' 
+    //                             + '<img src="${link}${no}.png">' 
+    //                         + '<li>';
 
-var num = 21;
-ig.on({
-    "append": function (_e) {
-        ig.append(getItems(num), _e.groupKey + 1);
-    },
-    "layoutComplete": function (_e) {
-        _e.target.forEach(function (item) {
-            if (!item.el) {
-                return;
-            }
-            // add animation
-            item.el.setAttribute("class", "item animate");
-        });
-        if (!_e.isAppend && _e.fromCache && _e.target[0].groupKey === 0) {
-            ig.layout(true);
+
+    var currentlyLoaded = 0;
+    // var template = '<li><img src="${link}${no}.png"><li>';
+    var template = '<li>${content}<li>';
+    var link = "./gallery-images/";
+    var content;
+    var num = 9;
+    // var num = 28;
+    // var num = 21;
+    function getItem(_template, _options) {
+        // return template.replace(/\$\{([^\}]*)\}/g, function () { //original
+        return _template.replace(/\$\{([^\}]*)\}/g, function () {
+            var replaceTarget = arguments[1];
+
+            return _options[replaceTarget];
+        }//end function
+        );
+    }//end getItem
+
+    function getItems(_length) {
+        var arr = [];
+
+        for (var i = 0; i < _length; ++i) {
+            arr.push(getItem(template, {
+                no: i % 60 + 1,
+                text: "egjs post " + (i + 1),
+                link: link,
+                content: allGalleryPosts[i].innerHTML
+            }));
         }
+        return arr;
     }
+
+    // ig.no(
+    //     {
+    //         "append": function (_e) {
+
+    //         }
+    //     }
+    // );
+    // ig.on({
+    //     "append": function (_e) {
+    //         ig.append(getItems(num), _e.groupKey + 1);
+    //     },
+    //     "layoutComplete": function (_e) {
+    //         _e.target.forEach(function (item) {
+    //             if (!item.el) {
+    //                 return;
+    //             }
+    //             // add animation
+    //             item.el.setAttribute("class", "item animate");
+    //         });
+    //         if (!_e.isAppend && _e.fromCache && _e.target[0].groupKey === 0) {
+    //             ig.layout(true);
+    //         }
+    //     }
+    // });
+
+    ig.append(getItems(num), 0);
+
+
 });
 
-ig.append(getItems(num * 2), 0);
