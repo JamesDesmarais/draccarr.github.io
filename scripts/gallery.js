61c16f3bcce2e225b51765fdf03f32e96af5d33b
Gavenmore <jamesdon10@hotmail.com>
2020-03-29 13:28:52 -0600

    Added some images and modified gallery.js

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -7,8 +7,8 @@ InfiniteGrids documentation:
 ***********************************************/
 var InfiniteGrid = eg.InfiniteGrid;
 var GridLayout = InfiniteGrid.GridLayout;
-var ig = new InfiniteGrid(document.getElementById("Grid"), {
-    horizontal: false,
+var ig = new InfiniteGrid(document.getElementById('Grid'), {
+	horizontal: false
 });
 // var parallax = new eg.Parallax(window, {
 //     container: ".art-container",
@@ -21,10 +21,10 @@ var ig = new InfiniteGrid(document.getElementById("Grid"), {
 // });
 
 ig.setLayout(GridLayout, {
-    horizontal: false,
-    itemSize: 285,
-    margin: 24,
-    align: "center"
+	horizontal: false,
+	itemSize: 285,
+	margin: 24,
+	align: 'center'
 });
 // if (window.innerWidth <= 1200) {
 //     ig.setLayout(GridLayout, {
@@ -45,157 +45,66 @@ ig.setLayout(GridLayout, {
 var galleryDoc;
 var allGalleryPosts;
 var currentGalleryPost;
-let allCurrentPosts = [];//Can be removed later.
-$.get("./gallery-content.html", function (_content) {
-    galleryDoc = new DOMParser().parseFromString(_content, "text/html");
-    allGalleryPosts = galleryDoc.getElementsByTagName("li");
+let allCurrentPosts = []; //Can be removed later.
+$.get('./gallery-content.html', function(_content) {
+	galleryDoc = new DOMParser().parseFromString(_content, 'text/html');
+	allGalleryPosts = galleryDoc.getElementsByTagName('li');
 
+	var template =
+		'<li class="card" onmouseover="this.classList.add(\'show-buttons\')" onmouseout="this.classList.remove(\'show-buttons\')">${content}<li>';
+	var link = './gallery-images/';
+	var num = allGalleryPosts.length;
+	var id = 0;
+	function getItem(_template, _options) {
+		return _template.replace(/\$\{([^\}]*)\}/g, function() {
+			var replaceTarget = arguments[1];
 
-    var template = '<li class="card" onmouseover="this.classList.add(\'show-buttons\')" onmouseout="this.classList.remove(\'show-buttons\')">${content}<li>';
-    var link = "./gallery-images/";
-    var num = 9;
-    var id = 0;
-    function getItem(_template, _options) {
-        return _template.replace(/\$\{([^\}]*)\}/g, function () {
-            var replaceTarget = arguments[1];
+			return _options[replaceTarget];
+		});
+	} //end getItem
 
-            return _options[replaceTarget];
-        }//end function
-        );
-    }//end getItem
+	function getItems(_length) {
+		var arr = [];
 
-    function getItems(_length) {
-        var arr = [];
+		for (var i = 0; i < _length; ++i) {
+			arr.push(
+				getItem(template, {
+					no: i % 60 + 1,
+					text: 'egjs post ' + (i + 1),
+					link: link,
+					content: allGalleryPosts[i].innerHTML
+				})
+			);
+		}
+		arr.forEach((_x) => allCurrentPosts.push(_x));
 
-        for (var i = 0; i < _length; ++i) {
-            arr.push(getItem(template, {
-                no: i % 60 + 1,
-                text: "egjs post " + (i + 1),
-                link: link,
-                content: allGalleryPosts[i].innerHTML, 
+		return arr;
+	} //end getItems
 
-            }));
-        }
-        arr.forEach(_x => allCurrentPosts.push(_x));
-        // arr.forEach(_x => console.log(typeof _x));
-        // arr.forEach(_x => _x.addEventListener('mouseenter', e => {
-        //     DisplayButtons();
-        // }));
+	// ig.on(
+	//     {
+	//         "append": function (_e) {
 
-        // AddMyListeners(arr);
+	//         }
+	//     }
+	// );
+	// ig.on({
+	//     "append": function (_e) {
+	//         ig.append(getItems(num), _e.groupKey + 1);
+	//     },
+	//     "layoutComplete": function (_e) {
+	//         _e.target.forEach(function (item) {
+	//             if (!item.el) {
+	//                 return;
+	//             }
+	//             // add animation
+	//             item.el.setAttribute("class", "item animate");
+	//         });
+	//         if (!_e.isAppend && _e.fromCache && _e.target[0].groupKey === 0) {
+	//             ig.layout(true);
+	//         }
+	//     }
+	// });
 
-        // mouseTarget.addEventListener('mouseenter', e => {
-        //     mouseTarget.style.border = '5px dotted orange';
-        //     enterEventCount++;
-        //     addListItem('This is mouseenter event ' + enterEventCount + '.');
-        // });
-
-
-        // console.log(allCurrentPosts);
-        return arr;
-    }
-
-    // ig.on(
-    //     {
-    //         "append": function (_e) {
-
-    //         }
-    //     }
-    // );
-    // ig.on({
-    //     "append": function (_e) {
-    //         ig.append(getItems(num), _e.groupKey + 1);
-    //     },
-    //     "layoutComplete": function (_e) {
-    //         _e.target.forEach(function (item) {
-    //             if (!item.el) {
-    //                 return;
-    //             }
-    //             // add animation
-    //             item.el.setAttribute("class", "item animate");
-    //         });
-    //         if (!_e.isAppend && _e.fromCache && _e.target[0].groupKey === 0) {
-    //             ig.layout(true);
-    //         }
-    //     }
-    // });
-
-    ig.append(getItems(num), 0);
-
-    // function ChangeLayoutOnWinResize() {
-    //     if (window.innerWidth <= 1200) {
-    //         ig.setLayout(GridLayout, {
-    //             horizontal: false,
-    //             itemSize: 300,
-    //             margin: 20,
-    //             align: "center"
-    //         })
-    //         console.log("I'm smaller than or queal to 1200 " + window.innerWidth);
-    //     } else {
-    //         ig.setLayout(GridLayout, {
-    //             horizontal: false,
-    //             itemSize: 250,
-    //             margin: 20,
-    //             align: "center"
-    //         })
-    //         console.log("I'm larger than 1200 " + window.innerWidth);
-    //     }
-    //     parallax.resize(getItems(num));
-    // }//end ChangeLayoutOnWinResize
-    // window.addEventListener('resize', ChangeLayoutOnWinResize);
+	ig.append(getItems(num), 0);
 });
-// function ChangeLayoutOnWinResize() {
-//     if (window.innerWidth <= 1200) {
-//         ig.setLayout(GridLayout, {
-//             horizontal: false,
-//             itemSize: 300,
-//             margin: 20,
-//             align: "center"
-//         })
-//         console.log("I'm smaller than or queal to 1200 " + window.innerWidth);
-//     } else {
-//         ig.setLayout(GridLayout, {
-//             horizontal: false,
-//             itemSize: 250,
-//             margin: 20,
-//             align: "center"
-//         })
-//         console.log("I'm larger than 1200 " + window.innerWidth);
-//     }
-//     parallax.resize(items);
-// }//end ChangeLayoutOnWinResize
-// window.addEventListener('resize', ChangeLayoutOnWinResize);
-
-
-// function AddMyListeners(_htmlStringArray) {
-//     let myElements = [];
-//     let myElementsOnPage = [];
-//     //Make new objects of each string as dom elements
-//     _htmlStringArray.forEach(_x => myElements.push(new DOMParser().parseFromString(_x, "text/html")));
-//     //find the images in each of the elements and grab their ids
-//     console.log(myElements);
-//     for (var i=0; i< myElements.length; i++) {
-//         // myElementsOnPage.push(document.getElementById(myElements[i].images[0].id));
-//         console.log(myElements[i].images[0].id);
-//     }
-//     // console.log(myElementsOnPage);
-
-//     // _htmlStringArray.forEach(_x => new DOMParser().parseFromString(_x, "text/html")
-//     //                                                                                         .addEventListener('mouseenter', e => {
-//     //     console.log(this);
-//     //     DisplayButtons(this);
-
-//     // }));
-// }
-
-// function DisplayButtons(_domElement) {
-//     console.log(_domElement);
-// }//end DisplayButtons
-
-// document.addEventListener('click', function (_e) {
-//     if (_e.target && _e.target.tagName == "IMG" && _e.target.id != "PageImage") {
-//         // _e.target.parent;
-//         console.log(_e.target.parentNode);
-
-//     }
-// });
