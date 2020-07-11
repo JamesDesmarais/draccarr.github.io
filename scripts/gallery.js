ad3440be959eb88a54d51c8ad215368adad9c3e7
Gavenmore <jamesdon10@hotmail.com>
2020-07-10 20:16:58 -0600

    Removed not needed scripts and pages. Renamed various pages

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,60 +1,96 @@
 /**********************************************
 Author: James Desmarais
-Date modified: Mar 16, 2020
+Date modified: Jul 03, 2020
 
 InfiniteGrids documentation:
     https://naver.github.io/egjs-infinitegrid/#layouts
 ***********************************************/
-var InfiniteGrid = eg.InfiniteGrid;
-var GridLayout = InfiniteGrid.GridLayout;
-var ig = new InfiniteGrid(document.getElementById('Grid'), {
-	horizontal: false
-});
+// var InfiniteGrid = eg.InfiniteGrid;
+// var GridLayout = InfiniteGrid.GridLayout;
+// var ig = new InfiniteGrid(document.getElementById('Grid'), {
+// 	horizontal: false
+// });
 
-ig.setLayout(GridLayout, {
-	horizontal: false,
-	itemSize: 285,
-	margin: 24,
-	align: 'center'
-});
+// ig.setLayout(GridLayout, {
+// 	horizontal: false,
+// 	itemSize: 285,
+// 	margin: 24,
+// 	align: 'center'
+// });
 
-var galleryDoc;
-var allGalleryPosts;
-var currentGalleryPost;
-let allCurrentPosts = []; //Can be removed later.
-$.get('./gallery-content.html', function(_content) {
-	galleryDoc = new DOMParser().parseFromString(_content, 'text/html');
-	allGalleryPosts = galleryDoc.getElementsByTagName('li');
+// const allImages = new DOMParser().parseFromString(loadPage('https://draccarr.github.io/gallery-content-v2.html'), 'text/html').querySelectorAll("[data-src]");
+// const imageOptions = {
+//     threshold: 1,
+//     rootMargin: "0px 0px 50px 0px"
+// };
+// const imageObserver = new IntersectionObserver((_entries, _imageObserver) => {
+//     _entries.forEach(_x => {
+//         if (!_x.isIntersecting) {
+//             return;
+//         } else {
+//             preloadImage(_x.target);
+//             imageObserver.unobserve(_x.target);
+//         }
+//     });
+// }, imageOptions);
 
-	var template =
-		'<li class="card" onmouseover="this.classList.add(\'show-buttons\')" onmouseout="this.classList.remove(\'show-buttons\')">${content}<li>';
-	var link = './gallery-images/';
-	var num = allGalleryPosts.length;
-	function getItem(_template, _options) {
-		return _template.replace(/\$\{([^\}]*)\}/g, function() {
-			var replaceTarget = arguments[1];
+// const myGrid = document.getElementById("Grid");
+// const allGalleryPosts = new DOMParser().parseFromString(loadPage('https://draccarr.github.io/gallery-content-v2.html'), 'text/html').getElementsByTagName("li");
+// let allCurrentPosts = []; //Can be removed later.
+// window.onload = function() {
 
-			return _options[replaceTarget];
-		});
-	} //end getItem
+//     var template = '<li class="card" onmouseover="this.classList.add(\'show-buttons\')" onmouseout="this.classList.remove(\'show-buttons\')">${content}<li>';
+// 	var link = './gallery-images/';
+//     var num = allGalleryPosts.length;
+    
+//     function getItem(_template, _options) {
+// 		return _template.replace(/\$\{([^\}]*)\}/g, function() {
+// 			var replaceTarget = arguments[1];
 
-	function getItems(_length) {
-		var arr = [];
+// 			return _options[replaceTarget];
+// 		});
+//     } //end getItem
+    
+//     function getItems(_length) {
+// 		var arr = [];
 
-		for (var i = 0; i < _length; ++i) {
-			arr.push(
-				getItem(template, {
-					no: i % 60 + 1,
-					text: 'egjs post ' + (i + 1),
-					link: link,
-					content: allGalleryPosts[i].innerHTML
-				})
-			);
-		}
-		arr.forEach((_x) => allCurrentPosts.push(_x));
+// 		for (var i = 0; i < _length; ++i) {
+// 			arr.push(
+// 				getItem(template, {
+// 					no: i % 60 + 1,
+// 					text: 'egjs post ' + (i + 1),
+// 					link: link,
+// 					content: allGalleryPosts[i].innerHTML
+// 				})
+// 			);
+// 		}
+// 		arr.forEach((_x) => allCurrentPosts.push(_x));
 
-		return arr;
-	} //end getItems
+// 		return arr;
+//     } //end getItems
+    
+//     ig.append(getItems(num), 0);
 
-	ig.append(getItems(num), 0);
-});
+    
+//     myGrid.querySelectorAll("[data-src]").forEach(_x => {
+//         imageObserver.observe(_x);
+//     });
+// }; //end window.onLoad
+
+// //Loads html pages
+// function loadPage(_href) {
+// 	var xmlhttp = new XMLHttpRequest();
+// 	xmlhttp.open('GET', _href, false);
+// 	xmlhttp.send();
+// 	return xmlhttp.responseText;
+// }//end loadPage
+
+// //Preloads the image
+// function preloadImage(_img) {
+//     const src = _img.getAttribute("data-src");
+//     if (!src) {
+//         return;
+//     } else {
+//         _img.src = src;
+//     }
+// }//end preloadImage
