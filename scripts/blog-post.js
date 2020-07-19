3b879cb94aa97022473fface15060739288fcaaf
Gavenmore <jamesdon10@hotmail.com>
2020-07-19 13:32:34 -0600

    Removed JQuery dependancy in blog-post.js

--- scripts/blog-post.js
+++ scripts/blog-post.js
@@ -1,16 +1,17 @@
 //Replace JQuery with this later.
-// window.addEventListener("load", initializeBlogPost, true); 
+window.addEventListener("load", initializeBlogPost, true); 
 var postsDoc;
 var allPosts;
 var currentPost;
-// function initializeBlogPost() {
-
-// }//end initializeBlogPost
-$.get('https://draccarr.github.io/blog-content.html', function(_content) {
-	postsDoc = new DOMParser().parseFromString(_content, 'text/html');
-	$('#BlogPost').load('blog-content.html ' + window.location.hash);
+function initializeBlogPost() {
+    fetch('https://draccarr.github.io/blog-content.html')
+        .then(response => response.text())
+        .then((data) => {
+        postsDoc = new DOMParser().parseFromString(data, "text/html");
+		document.querySelector('#BlogPost').innerHTML = postsDoc.querySelector(window.location.hash).outerHTML;
+	});
 	ReplaceAnchors();
-});
+}//end initializeBlogPost
 
 var previousPostAnchorTop;
 var previousPostAnchorBottom;
@@ -18,55 +19,61 @@ var nextPostAnchorTop;
 var nextPostAnchorBottom;
 var loadedHTML;
 ReplaceAnchors = function() {
-	if (document.location.hash.length > 0) {
-		previousPostAnchorTop = document.getElementById('PreviousPostTop');
-		previousPostAnchorBottom = document.getElementById('PreviousPostBottom');
-		nextPostAnchorTop = document.getElementById('NextPostTop');
-		nextPostAnchorBottom = document.getElementById('NextPostBottom');
-		if (previousPostAnchorTop && previousPostAnchorBottom) {
-			allPosts = postsDoc.getElementsByTagName('article');
-			for (var i = 0; i < allPosts.length; i++) {
-				if ('#' + allPosts[i].id == location.hash) {
-					if (allPosts.length > i + 1) {
-						previousPostAnchorTop.outerHTML =
-							'<a id="PreviousPost" title="Previous" href="' +
-							'?' +
-							i +
-							'=./blog-post.html#' +
-							allPosts[i + 1].id +
-							'"><i class="fas fa-arrow-left"></i></a>';
-						previousPostAnchorBottom.outerHTML =
-							'<a id="PreviousPost" title="Previous" onclick="changeSeriesPosition(-1)" href="' +
-							'?' +
-							i +
-							'=./blog-post.html#' +
-							allPosts[i + 1].id +
-							'"><i class="fas fa-arrow-left"></i></a>';
-					} else {
-						previousPostAnchorTop.outerHTML = '<a id="PreviousPost"></a>';
-						previousPostAnchorBottom.outerHTML = '<a id="PreviousPost"></a>';
-					}
-					if (i - 1 >= 0) {
-						nextPostAnchorTop.outerHTML =
-							'<a id="NextPost" title="Next" href="' +
-							'?' +
-							i +
-							'=./blog-post.html#' +
-							allPosts[i - 1].id +
-							'"><i class="fas fa-arrow-right"></i></a>';
-						nextPostAnchorBottom.outerHTML =
-							'<a id="NextPost" title="Next" onclick="changeSeriesPosition(1)" href="' +
-							'?' +
-							i +
-							'=./blog-post.html#' +
-							allPosts[i - 1].id +
-							'"><i class="fas fa-arrow-right"></i></a>';
-					} else {
-						nextPostAnchorTop.outerHTML = '<a id="NextPost"></a>';
-						nextPostAnchorBottom.outerHTML = '<a id="NextPost"></a>';
+	    fetch('https://draccarr.github.io/blog-content.html')
+        .then(response => response.text())
+        .then((data) => {
+		postsDoc = new DOMParser().parseFromString(data, "text/html");
+		if (document.location.hash.length > 0) {
+			previousPostAnchorTop = document.getElementById('PreviousPostTop');
+			previousPostAnchorBottom = document.getElementById('PreviousPostBottom');
+			nextPostAnchorTop = document.getElementById('NextPostTop');
+			nextPostAnchorBottom = document.getElementById('NextPostBottom');
+			if (previousPostAnchorTop && previousPostAnchorBottom) {
+				allPosts = postsDoc.getElementsByTagName('article');
+				for (var i = 0; i < allPosts.length; i++) {
+					if ('#' + allPosts[i].id == location.hash) {
+						if (allPosts.length > i + 1) {
+							previousPostAnchorTop.outerHTML =
+								'<a id="PreviousPost" title="Previous" href="' +
+								'?' +
+								i +
+								'=./blog-post.html#' +
+								allPosts[i + 1].id +
+								'"><i class="fas fa-arrow-left"></i></a>';
+							previousPostAnchorBottom.outerHTML =
+								'<a id="PreviousPost" title="Previous" onclick="changeSeriesPosition(-1)" href="' +
+								'?' +
+								i +
+								'=./blog-post.html#' +
+								allPosts[i + 1].id +
+								'"><i class="fas fa-arrow-left"></i></a>';
+						} else {
+							previousPostAnchorTop.outerHTML = '<a id="PreviousPost"></a>';
+							previousPostAnchorBottom.outerHTML = '<a id="PreviousPost"></a>';
+						}
+						if (i - 1 >= 0) {
+							nextPostAnchorTop.outerHTML =
+								'<a id="NextPost" title="Next" href="' +
+								'?' +
+								i +
+								'=./blog-post.html#' +
+								allPosts[i - 1].id +
+								'"><i class="fas fa-arrow-right"></i></a>';
+							nextPostAnchorBottom.outerHTML =
+								'<a id="NextPost" title="Next" onclick="changeSeriesPosition(1)" href="' +
+								'?' +
+								i +
+								'=./blog-post.html#' +
+								allPosts[i - 1].id +
+								'"><i class="fas fa-arrow-right"></i></a>';
+						} else {
+							nextPostAnchorTop.outerHTML = '<a id="NextPost"></a>';
+							nextPostAnchorBottom.outerHTML = '<a id="NextPost"></a>';
+						}
 					}
 				}
 			}
-		}
-	}
-};
+		}	
+	});
+}//end ReplaceAnchors
+
