affd34a8d7301c0fa715b8b9f50954464cc0994b
Gavenmore <jamesdon10@hotmail.com>
2020-07-18 20:25:59 -0600

    manifest stuff

--- scripts/main.js
+++ scripts/main.js
@@ -27,9 +27,37 @@ window.onload = function() {
 		}
 	} catch (_ex) {}
 }; //end onload
-// $(function() {
-// 	// $('header').load('header.html');
-// 	// $('#fancyNav').load('main-nav.html');
+
+// window.addEventListener('beforeinstallprompt', (event) => {
+//   console.log('👍', 'beforeinstallprompt', event);
+//   // Stash the event so it can be triggered later.
+//   window.deferredPrompt = event;
+//   // Remove the 'hidden' class from the install button container
+//   divInstall.classList.toggle('hidden', false);
+// });
+
+// document.getElementById("BtnInstall").addEventListener('click', () => {
+//   console.log('👍', 'butInstall-clicked');
+//   const promptEvent = window.deferredPrompt;
+//   if (!promptEvent) {
+//     // The deferred prompt isn't available.
+//     return;
+//   }
+//   // Show the install prompt.
+//   promptEvent.prompt();
+//   // Log the result
+//   promptEvent.userChoice.then((result) => {
+//     console.log('👍', 'userChoice', result);
+//     // Reset the deferred prompt variable, since
+//     // prompt() can only be called once.
+//     window.deferredPrompt = null;
+//     // Hide the install button.
+//     divInstall.classList.toggle('hidden', true);
+//   });
+// });
+
+// window.addEventListener('appinstalled', (event) => {
+//   console.log('👍', 'appinstalled', event);
 // });
 
 //====================
